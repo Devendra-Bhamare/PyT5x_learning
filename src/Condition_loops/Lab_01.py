9c6fd944337f30d4891d53172e0d8370a6251216
Devendra's codes <bhamaredevendra777@gmail.com>
2025-02-15 23:15:50 +0530

    loop

---
+++ src/Condition_loops/Lab_01.py
@@ -0,0 +1,12 @@
+num1 = float (input("Enter 1st number"))
+num2 = float (input("Enter 2nd number"))
+
+if num1 > 20:
+    print("Ok enter into the clg")
+else:
+    print("U r not 20 yrs old")
+
+if num2 > 12:
+    print("Ok enter into the clg")
+else:
+     print("U r not 20 yrs old")
