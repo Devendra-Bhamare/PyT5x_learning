7e1f4a551c5d0240dd280d2d94c8b5e72794c06e
Devendra's codes <bhamaredevendra777@gmail.com>
2025-03-01 00:28:21 +0530

    operators

---
+++ Python_/Loops/Lab03_for_if_else.py
@@ -0,0 +1,16 @@
+for i in range(0, 10):
+    if i == 5:
+        print("Five")
+    else:
+        print(i)
+
+
+    #OutPUT
+    #| i | Condition | O/p
+    # |0| 0==5 -> False | o/p = 0
+    # |0| 1==5 -> False | o/p = 1
+    # |0| 2==5 -> False | o/p = 2
+    # |0| 3==5 -> False | o/p = 3
+    # |0| 4==5 -> False | o/p = 4
+    # |0| 5==5 -> True | o/p = Five
+    # |0| 6==5 -> False | o/p = 6
---
+++ Python_/Loops/Lab_03_Pass.py
@@ -0,0 +1,12 @@
+for i in range(0, 10):
+    if i == 5 or i==4:
+        print(i)
+    else:
+        pass
+
+    # OutPUT
+    # | i | Condition | O/p
+    # |0| 0==5/4 -> False | o/p = nothing will be printed kahich print honr nhi PASS aahe mhun
+    # |0| 1==5 -> False | o/p = nothing will be printed kahich print honr nhi PASS aahe mhun
+    # |0| 2==5 -> False | o/p = nothing will be printed kahich print honr nhi PASS aahe mhun
+    # |0| 3==5 -> False | o/p = nothing will be printed kahich print honr nhi PASS aahe mhun
